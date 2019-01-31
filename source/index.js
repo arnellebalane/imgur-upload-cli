396e13f7b0cf2ff244fb9240a960282e90fdb070
Arnelle Balane <arnellebalane@gmail.com>
2019-01-31 10:41:38 +0800

    Define all method outlines in index.js

---
+++ source/index.js
@@ -0,0 +1,31 @@
+import fs from 'fs';
+import path from 'path';
+import fetch from 'fetch';
+
+export function uploadImage(imagePath) {
+
+}
+
+export function uploadAlbum(imagePaths) {
+
+}
+
+export function displayHistory() {
+
+}
+
+export function clearHistory() {
+
+}
+
+export function setBaseDirectory(baseDirPath) {
+
+}
+
+export function getBaseDirectory() {
+
+}
+
+export function uploadLatest(dirPath) {
+
+}
