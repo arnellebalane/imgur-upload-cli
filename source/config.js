2a5f90ef941a2b0c6d89b24ba698cf7542f64df3
Liri <liris@diagnosticrobotics.com>
2019-06-20 10:13:41 +0300

    Added IMGUR_CLIENT_ID configurable variable option

--- source/config.js
+++ source/config.js
@@ -1,5 +1,5 @@
 export default {
-    API_CLIENT_ID: 'ae51d45d93313f1',
+    DEFAULT_API_CLIENT_ID: 'ae51d45d93313f1',
     API_IMAGE_URL: 'https://api.imgur.com/3/image',
     API_ALBUM_URL: 'https://api.imgur.com/3/album'
 };
