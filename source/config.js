8728f61096d88999a385d8c5a962d99e2638ed76
Liri <liris@diagnosticrobotics.com>
2019-07-09 11:41:41 +0300

    Single line condition

--- source/config.js
+++ source/config.js
@@ -1,16 +1,7 @@
-
 const DEFAULT_API_CLIENT_ID = 'ae51d45d93313f1';
 
-function getApiClientId() {
-    let apiClientId = process.env.IMGUR_CLIENT_ID;
-    if (!apiClientId) {
-        apiClientId = DEFAULT_API_CLIENT_ID;
-    }
-    return apiClientId;
-}
-
 export default {
-    API_CLIENT_ID: getApiClientId(),
+    API_CLIENT_ID: process.env.IMGUR_CLIENT_ID || DEFAULT_API_CLIENT_ID,
     API_IMAGE_URL: 'https://api.imgur.com/3/image',
     API_ALBUM_URL: 'https://api.imgur.com/3/album',
 };
